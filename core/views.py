866247a5affb091a61c4360280b139163a8cab82
Darixxxon <dmorzuch@wp.pl>
2025-05-25 13:39:59 +0200

    Add csrf decorator

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,  redirect,  get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from .models import Wallet, CurrentAsset, WalletAsset, Asset
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
