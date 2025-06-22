c0bdd9bedbee030ee37c5b782d3a8731678f0e8e
MaksPek03 <maks.pek90@gmail.com>
2025-06-22 14:16:56 +0200

    adding friend to the models

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.conf import settings
+from django.utils import timezone
 
 class Asset(models.Model):
     name = models.CharField(max_length = 50)
@@ -31,4 +33,63 @@ class WalletAsset(models.Model):
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
     purchase_date = models.DateField()
+
+class FriendList(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user')
+    friends = models.ManyToManyField(settings.AUTH_USER_MODEL, blank = True, related_name='friends')
+
+    def __str__(self):
+        return self.user.username
+    def add_friend(self, account):
+        if not account in self.friends.all():
+            self.friends.add(account)
+            self.save()
+
+    def remove_friend(self, account):
+        if account in self.friends.all():
+            self.friends.remove(account)
+            self.save()
+
+    def unfriend(self, removee):
+        remover_friends_list = self # person executing the friendship
+        remover_friends_list.remove_friend(removee) # remove friend from remover friend list
+
+        # remove friend from removee friend list
+        friends_list = FriendList.objects.get(user=removee)
+        friends_list.remove_friend(self.user)
+
+    def is_mutual_friend(self, friend):
+        if friend in self.friends.all():
+            return True
+        return False
     
+
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name = 'receiver')
+    is_active = models.BooleanField(blank=True, null=False, default=True)
+
+    timestamp = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.sender.username
+    
+    def accept(self):
+        receiver_friend_list = FriendList.objects.get(user=self.receiver)
+        if receiver_friend_list:
+            receiver_friend_list.add_friend(self.sender)
+            sender_friend_list = FriendList.objects.get(user=self.sender)
+            if sender_friend_list:
+                sender_friend_list.add_friend(self.receiver)
+                self.is_active = False
+                self.save()
+
+    def decline(self):
+        self.is_active = False
+        self.save()
+
+    # cancelling is not the same as declining, 
+    # cancelling is when the sender wants to cancel the request before it is accepted or declined
+    def cancel(self):     
+        self.is_active = False
+        self.save() 
