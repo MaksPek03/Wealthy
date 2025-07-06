80dc39312380ee82e447296451771c01897a1634
Darixxxon <dmorzuch@wp.pl>
2025-07-06 19:27:14 +0200

    Add a section with graphs of historic prices

---
+++ frontend/app/checktrends/checktrends.tsx
@@ -0,0 +1,107 @@
+import {View, Text, TouchableOpacity, ActivityIndicator, FlatList, TextInput} from 'react-native';
+import { useRouter} from 'expo-router';
+import { useTheme } from "@/app/context/ThemeContext";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, {useEffect, useState} from 'react';
+
+interface Price {
+    name: string;
+    symbol: string;
+    current_price: number;
+}
+
+export default function MainMenu() {
+    const { isDark, toggleTheme } = useTheme();
+    const router = useRouter();
+    const [prices, setPrices] = useState<Price[]>([]);
+    const [filteredPrices, setFilteredPrices] = useState<Price[]>([]);
+    const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        fetchListOfAssets();
+    }, []);
+
+    const fetchListOfAssets = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('https://wealthy-0mga.onrender.com/api/price/');
+            const data: Price[] = await response.json();
+
+            const sorted = [...data].sort((a, b) => a.name.localeCompare(b.name));
+
+            setPrices(sorted);
+        } catch (err) {
+            console.error('Error fetching prices:', err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleSearch = (text: string) => {
+        setSearch(text);
+        const filtered = prices.filter(item =>
+            item.name.toLowerCase().includes(text.toLowerCase())
+        );
+        setFilteredPrices(filtered);
+    };
+
+    const renderHeader = () => (
+        <View className={`flex-row ${isDark ? "bg-buttons-dark" : "bg-buttons"} p-2 px-4 border-b border-gray-300`}>
+            <Text className={`font-bold text-xl flex-1 ${isDark ? "text-text-dark" : "text-text"}`}>
+                Name
+            </Text>
+            <Text className={`font-bold text-xl flex-1 ${isDark ? "text-text-dark" : "text-text"}`}>
+                Symbol
+            </Text>
+        </View>
+    );
+
+
+    return (
+        <SafeAreaView className={`flex-1 ${isDark ? "bg-headers-dark" : "bg-headers"}`}>
+            <View className={`flex-[1] justify-center items-center relative ${isDark ? "bg-headers-dark" : "bg-headers"}`}>
+                <Text className={`text-6xl ${isDark ? "text-headers-text-dark" : "text-headers-text"}`}>CHECK TRENDS</Text>
+            </View>
+            <View className={`flex-[5] ${isDark ? "bg-background-dark" : "bg-background"}`}>
+                <TextInput
+                    className={`p-2 mb-2 ${isDark ? "text-text-dark" : "text-text"}`}
+                    placeholder="Search by name..."
+                    value={search}
+                    onChangeText={handleSearch}
+                />
+
+                {loading ? (
+                    <ActivityIndicator size="large" />
+                ):(
+                    <FlatList
+                        data={filteredPrices.length > 0 || search ? filteredPrices : prices}
+                        keyExtractor={(item) => item.symbol}
+                        ListHeaderComponent={renderHeader}
+                        stickyHeaderIndices={[0]}
+                        renderItem={({item}) => (
+                            <TouchableOpacity
+                                onPress={() => router.push(`/checktrends/trends/${item.symbol}`)}
+                            >
+                                <View className={`flex-row py-2 border-b-2 border-gray-300 px-4`}>
+                                    <Text className={`flex-1 text-2xl ${isDark ? "text-text-dark" : "text-text"}`}>{item.name}</Text>
+                                    <Text className={`flex-1 text-2xl ${isDark ? "text-text-dark" : "text-text"}`}>{item.symbol}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        )}
+                    />
+                )
+                }
+            </View>
+
+            <View className={`flex-[1] justify-center items-center ${isDark ? "bg-headers-dark" : "bg-headers"}`}>
+                <TouchableOpacity
+                    onPress={() => router.back()}
+                    className={`px-8 py-4 rounded-3xl min-h-10 min-w-40 mb-8 ${isDark ? "bg-buttons-dark" : "bg-buttons"}`}
+                >
+                    <Text className={`text-1xl text-center font-bold ${isDark ? "text-text-dark" : "text-text"}`}>GO BACK</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
---
+++ frontend/app/components/filterHistory.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, Pressable } from 'react-native';
+import { useTheme } from "@/app/context/ThemeContext";
+
+const FilterHistory = (props: { filterDay: any; filterText: any; selectedFilter: any; setSelectedFilter: any; }) => {
+    const { filterDay, filterText, selectedFilter, setSelectedFilter } = props;
+    const { isDark, toggleTheme } = useTheme();
+    const isSelectedFilter = (filter: string) => filter === selectedFilter;
+
+    return (
+        <Pressable className={`${isSelectedFilter(filterDay) ? "bg-selected" : "bg-transparent"} py-2.5 px-2.5 rounded-xl`}
+            onPress={() => {setSelectedFilter(filterDay)}}>
+            <Text className={`text-white`}>{filterText}</Text>
+        </Pressable>
+    )
+}
+
+export default FilterHistory;
