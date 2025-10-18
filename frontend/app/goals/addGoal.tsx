86f0b1cc719c2dc4a2d2baf64c020dbc11727f86
Dariusz Morzuch <dmorzuch@wp.pl>
2025-10-18 10:34:52 +0200

    Add components to manage goals

---
+++ frontend/app/goals/addGoal.tsx
@@ -0,0 +1,174 @@
+import {useTheme} from "@/app/context/ThemeContext";
+import {useRouter} from "expo-router";
+import Header from "@/app/components/Header";
+import React, {useState} from "react";
+import Footer from "@/app/components/Footer";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {ActivityIndicator, Alert, FlatList, Text, TextInput, TouchableOpacity, View} from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const addGoal = () => {
+    const { isDark, toggleTheme } = useTheme();
+    const router = useRouter();
+    const [ name, setName ] = useState("");
+    const [ description, setDescription ] = useState("");
+    const [ targetAmount, setTargetAmount ] = useState("");
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [deadline, setDeadline] = useState<Date | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    const showDatePicker = () => setDatePickerVisibility(true);
+    const hideDatePicker = () => setDatePickerVisibility(false);
+
+    const handleConfirmDate = (date: Date) => {
+        setDeadline(date);
+        hideDatePicker();
+    };
+
+    const handleAddGoal = async (name:string, amountParsed:number, description:string, formattedDate: string) => {
+        try {
+            const userId = await AsyncStorage.getItem('user_id');
+            const response = await fetch(
+                `https://wealthy-0mga.onrender.com/api/goals/add/`,
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        name: name,
+                        description: description,
+                        targetAmount: amountParsed,
+                        deadline: formattedDate,
+                        userId: userId,
+                    }),
+                }
+            );
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                Alert.alert("Error", data.error || "Failed to add goal");
+                return;
+            }
+
+            Alert.alert("Success", "Goal added successfully!");
+            router.replace(`/goals`);
+
+        } catch (err) {
+            console.error(err);
+            Alert.alert("Error", "Could not connect to server");
+        }
+    };
+
+    const onConfirm = () => {
+        const amountParsed = parseFloat(targetAmount.replace(',', '.'));
+
+        if (isNaN(amountParsed)) {
+            Alert.alert("Invalid input", "Amount must be numeric values.");
+            return;
+        }
+
+        if (name === "") {
+            Alert.alert("Missing name", "Please add name.");
+            return;
+        }
+
+        if (!deadline) {
+            Alert.alert("Missing date", "Please select a deadline date.");
+            return;
+        }
+
+        const formattedDate = deadline.toISOString().split('T')[0];
+
+        handleAddGoal(name, amountParsed, description, formattedDate)
+    };
+
+    return (
+        <SafeAreaView className={`flex-1 ${isDark ? "bg-headers-dark" : "bg-headers"}`}>
+            <Header title={"ADD GOAL"} />
+
+            <View className={`flex-[5] items-center ${isDark ? "bg-background-dark" : "bg-background"}`}>
+
+                <View className={`w-1/2`}>
+                    <Text className={`text-1xl text-center mt-12 font-bold ${isDark ? "text-text-dark" : "text-text"}`}>
+                        Name:
+                    </Text>
+
+                    <TextInput
+                        className={`px-8 py-4 min-h-14 min-w-60 text-center text-lg font-bold ${isDark ?
+                            "bg-buttons-dark text-text-dark" : "bg-buttons text-text"}`}
+                        placeholder={"Name:"}
+                        placeholderTextColor={"#000000"}
+                        value={name}
+                        onChangeText={setName}
+                        autoCapitalize="none"
+                    />
+
+                    <Text className={`text-1xl text-center mt-12 font-bold ${isDark ? "text-text-dark" : "text-text"}`}>
+                        Description:
+                    </Text>
+
+                    <TextInput
+                        className={`px-8 py-4 min-h-14 min-w-60 text-center text-lg font-bold ${isDark ?
+                            "bg-buttons-dark text-text-dark" : "bg-buttons text-text"}`}
+                        placeholder={"Description:"}
+                        placeholderTextColor={"#000000"}
+                        value={description}
+                        onChangeText={setDescription}
+                        autoCapitalize="none"
+                    />
+
+                    <Text className={`text-1xl text-center mt-12 font-bold ${isDark ? "text-text-dark" : "text-text"}`}>
+                        Target amount:
+                    </Text>
+
+                    <TextInput
+                        className={`px-8 py-4 min-h-14 min-w-60 text-center text-lg font-bold ${isDark ?
+                            "bg-buttons-dark text-text-dark" : "bg-buttons text-text"}`}
+                        placeholder={"Target amount:"}
+                        placeholderTextColor={"#000000"}
+                        value={targetAmount}
+                        onChangeText={setTargetAmount}
+                        autoCapitalize="none"
+                    />
+
+                    <Text className={`text-1xl text-center mt-12 font-bold ${isDark ? "text-text-dark" : "text-text"}`}>
+                        Purchase date:
+                    </Text>
+
+                    <TouchableOpacity
+                        onPress={showDatePicker}
+                        className={`px-8 py-4 rounded-3xl min-h-10 min-w-48 ${isDark ? "bg-buttons-dark" : "bg-buttons"}`}
+                    >
+                        <Text className={`text-1xl text-center font-bold ${isDark ? "text-text-dark" : "text-text"}`}>
+                            {deadline ? deadline.toLocaleDateString() : "Pick deadline"}
+                        </Text>
+                    </TouchableOpacity>
+
+                    <DateTimePickerModal
+                        isVisible={isDatePickerVisible}
+                        mode="date"
+                        onConfirm={handleConfirmDate}
+                        onCancel={hideDatePicker}
+                    />
+
+                    <TouchableOpacity
+                        disabled={loading}
+                        onPress={onConfirm}
+                        className={`px-8 py-4 rounded-3xl min-h-10 min-w-32 mt-14 ${isDark ? "bg-buttons-dark" : "bg-buttons"}`}
+                    >
+                        <Text className={`text-1xl text-center font-bold ${isDark ? "text-text-dark" : "text-text"}`}>
+                            CONFIRM
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+
+            <Footer path={`/goals`}/>
+
+        </SafeAreaView>
+    );
+}
+
+export default addGoal;
---
+++ frontend/app/goals/index.tsx
@@ -0,0 +1,137 @@
+
+import {useTheme} from "@/app/context/ThemeContext";
+import {router, useRouter} from "expo-router";
+import React, {useEffect, useState} from "react";
+import {SafeAreaView} from "react-native-safe-area-context";
+import Header from "@/app/components/Header";
+import {ActivityIndicator, Alert, FlatList, Text, TouchableOpacity, View} from "react-native";
+import Footer from "@/app/components/Footer";
+import CurrencyChange from "@/app/components/CurrencyChange";
+
+interface Goal {
+    goal_id: string;
+    name: string;
+    targetAmount: number;
+    totalValue: number;
+    deadline: string;
+    percent: number;
+}
+
+const goals = () => {
+    const { isDark, toggleTheme } = useTheme();
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const [goals, setGoals] = useState<Goal[]>([]);
+    const [exchangeRate, setExchangeRate] = useState(1);
+    const [currencySymbol, setCurrencySymbol] = useState("$");
+
+
+    useEffect(() => {
+        fetchGoals();
+    }, []);
+
+    const fetchGoals = async () => {
+        setLoading(true);
+        try {
+            const  response = await fetch(`https://wealthy-0mga.onrender.com/api/goals/`)
+            const data = await response.json();
+
+            console.log(data);
+
+            setGoals(data['goalsList']);
+        } catch (err) {
+            console.error('Error fetching goals', err);
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
+    const handleRemoveGoal = async (goal_id: string) => {
+        Alert.alert(
+            "Confirmation",
+            "Are you sure you want to remove this goal?",
+            [
+                {
+                    text: "Yes",
+                    onPress: async () => {
+                        try {
+                            const response = await fetch(`https://wealthy-0mga.onrender.com/api/goals/delete/${goal_id}/`)
+                        } catch (err) {
+                            console.error('Error removing goal:', err);
+                        } finally {
+                            router.replace(`/goals`);
+                        }
+                    }
+                },
+                {
+                    text: "No",
+                    style: "cancel",
+                }
+            ]
+        )
+    }
+
+    const onSelectedExchangeRate = (selectedExchangeRate: number) => {
+        setExchangeRate(selectedExchangeRate);
+    }
+
+    const onSelectedCurrencySymbol = (selectedCurrencySymbol: string) => {
+        setCurrencySymbol(selectedCurrencySymbol);
+    }
+
+    return (
+        <SafeAreaView className={`flex-1 ${isDark ? "bg-headers-dark" : "bg-headers"}`}>
+            <Header title={"GOALS"} />
+
+            <View className={`flex-[5] w-full ${isDark ? "bg-background-dark" : "bg-background"}`}>
+                <CurrencyChange setExchangeRate={onSelectedExchangeRate} setCurrencySymbol={onSelectedCurrencySymbol} />
+
+                {loading ? (
+                    <ActivityIndicator size="large" />
+                ):(
+                    <FlatList
+                        data={goals}
+                        keyExtractor={(item) => item.goal_id.toString()}
+                        ListEmptyComponent={
+                            <Text className={`text-xl text-center ${isDark ? "text-text-dark" : "text-text"}`}>
+                                You don't have any goals!
+                            </Text>
+                        }
+                        renderItem={({item}) => (
+                            <View className={`flex-row py-2 border-b-2 border-gray-300 px-4`}>
+                                <Text className={`flex-1 text-2xl ${isDark ? "text-text-dark" : "text-text"}`}>
+                                    {item.name}{"\n"}
+                                    Current value: {(item.totalValue / exchangeRate).toFixed(2)}{currencySymbol}{"\n"}
+                                    Target value: {(item.targetAmount / exchangeRate).toFixed(2)}{currencySymbol}{"\n"}
+                                    Percentage: {item.percent}%{"\n"}
+                                    deadline: {item.deadline}
+                                </Text>
+                                <TouchableOpacity
+                                    className={`py-3 px-2.5 m-3 max-h-10 ${isDark ? "bg-buttons-dark" : "bg-buttons"}`}
+                                    onPress={() =>handleRemoveGoal(item.goal_id)}
+                                >
+                                    <Text>
+                                        DEL
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                    />
+                )
+                }
+                <TouchableOpacity
+                    onPress={() => router.replace(`/goals/addGoal`)}
+                    className={`px-8 py-4 min-h-14 min-w-72 ${isDark ? "bg-buttons-dark" : "bg-buttons"}`}
+                >
+                    <Text className={`text-3xl text-center font-bold ${isDark ? "text-text-dark" : "text-text"}`}>ADD GOAL</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Footer path={`/menu`}/>
+
+        </SafeAreaView>
+    );
+}
+
+export default goals;
