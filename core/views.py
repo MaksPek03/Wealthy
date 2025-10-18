9441e485f690571697179a812e27fe9cbe2416a5
Dariusz Morzuch <dmorzuch@wp.pl>
2025-10-18 08:57:14 +0200

    add view and url to see goals

--- core/views.py
+++ core/views.py
@@ -144,6 +144,37 @@ def api_wallets(request, user_id):
     wallets = Wallet.objects.filter(user=user_id).values('id', 'name')
     return JsonResponse(list(wallets), safe=False)
 
+def api_user_goals(request):
+    goals = UserGoal.objects.filter(user=request.user)
+
+    goals_list = []
+    for goal in goals:
+        goal_id = goal_id
+        name = goal.name
+        targetAmount = goal.target_amount
+        totalValue = sum(
+                wallet.quantity * CurrentAsset.objects.get(symbol=wallet.asset.symbol).current_price
+                for wallet in WalletAsset.objects.filter(wallet__user=request.user)
+            )
+        deadline = goal.deadline
+        percent = 100 if total_value >= target_amount else round(total_value/target_amount*100, 2)
+
+
+        goals_list.append({
+            "goal_id": goal_id,
+            "name": name,
+            "targetAmount": targetAmount,
+            "totalValue": totalValue,
+            "deadline": deadline,
+            "percent": percent
+        })
+
+    data = {
+        "goalsList": goals_list
+    }
+
+    return JsonResponse(data, safe=False)
+
 # it creates the wallet, with a specific name for a given user
 @csrf_exempt
 def api_add_wallet(request):
